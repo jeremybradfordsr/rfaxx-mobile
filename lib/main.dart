e63ab3f159fd056c2e81b277afe10193abe6d9f2
Jeremy Bradford <jeremy@Jeremys-iMac.local>
2019-11-21 10:48:34 -0600

    RFaxx 0.9 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: RFaxxExplorer()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class RFaxxExplorer extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _RFaxxExplorerState createState() => _RFaxxExplorerState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _RFaxxExplorerState extends State<RFaxxExplorer> {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Set<String> _favorites = Set<String>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('RFaxx Mobile'),
+        // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
+        actions: <Widget>[
+          NavigationControls(_controller.future),
+          Menu(_controller.future, () => _favorites),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: WebView(
+        initialUrl: 'http://rfaxx.tk/index.html',
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+      floatingActionButton: _bookmarkButton(),
+    );
+  }
+
+  _bookmarkButton() {
+    return FutureBuilder<WebViewController>(
+      future: _controller.future,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (controller.hasData) {
+          return FloatingActionButton(
+            onPressed: () async {
+              var url = await controller.data.currentUrl();
+              _favorites.add(url);
+              Scaffold.of(context).showSnackBar(
+                SnackBar(content: Text('Saved $url for later.')),
+              );
+            },
+            child: Icon(Icons.favorite),
+          );
+        }
+        return Container();
+      },
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  Menu(this._webViewControllerFuture, this.favoritesAccessor);
+  final Future<WebViewController> _webViewControllerFuture;
+  // TODO(efortuna): Come up with a more elegant solution for an accessor to this than a callback.
+  final Function favoritesAccessor;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (!controller.hasData) return Container();
+        return PopupMenuButton<String>(
+          onSelected: (String value) async {
+            if (value == 'Email link') {
+              var url = await controller.data.currentUrl();
+              await launch(
+                  'mailto:?subject=Check out this =$url');
+            } else {
+              var newUrl = await Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return FavoritesPage(favoritesAccessor());
+                  }));
+              Scaffold.of(context).removeCurrentSnackBar();
+              if (newUrl != null) controller.data.loadUrl(newUrl);
+            }
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
+            const PopupMenuItem<String>(
+              value: 'Email link',
+              child: Text('Email link'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            const PopupMenuItem<String>(
+              value: 'See Favorites',
+              child: Text('See Favorites'),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  FavoritesPage(this.favorites);
+  final Set<String> favorites;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Favorite pages')),
+      body: ListView(
+          children: favorites
+              .map((url) => ListTile(
+              title: Text(url), onTap: () => Navigator.pop(context, url)))
+              .toList()),
+    );
+  }
+}
+
+class NavigationControls extends StatelessWidget {
+  const NavigationControls(this._webViewControllerFuture)
+      : assert(_webViewControllerFuture != null);
+
+  final Future<WebViewController> _webViewControllerFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<WebViewController>(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+        final bool webViewReady =
+            snapshot.connectionState == ConnectionState.done;
+        final WebViewController controller = snapshot.data;
+        return Row(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: true),
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: false),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  navigate(BuildContext context, WebViewController controller,
+      {bool goBack: false}) async {
+    bool canNavigate =
+    goBack ? await controller.canGoBack() : await controller.canGoForward();
+    if (canNavigate) {
+      goBack ? controller.goBack() : controller.goForward();
+    } else {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+            content: Text("No ${goBack ? 'back' : 'forward'} history item")),
+      );
+    }
   }
 }
